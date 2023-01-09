e1237e54456c38c46f15a551e37b56a0329c5122
Abhishek4298 <abhishek.bhavsar@bacancy.com>
2023-01-10 01:09:16 +0530

    If query currency not passed than showing missing parameter status code.

--- server.js
+++ server.js
@@ -10,14 +10,21 @@ app.use(express.json())
 app.get('/tokens', async (req, res) => {
 	// get dynamic currency
 	const currency = req.query.currency
+	if (!currency) {
+		return res
+			.status(400)
+			.send({ error: 'Missing required parameter: currency' })
+	}
 	try {
 		// get all tokens from etherspot
 		const tokens = await etherspot.getTokenListTokens()
-		let tokenItems = tokens.map((t) => t.symbol).splice(0,500)
+		let tokenItems = tokens.map((t) => t.symbol).slice(0, 500)
 
 		if (tokenItems.length) {
 			// coinGecko URL to get dynamic currency record
-			const coinGeckoUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${tokenItems.join(',')}&vs_currencies=${currency}`
+			const coinGeckoUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${tokenItems.join(
+				',',
+			)}&vs_currencies=${currency}`
 			const response = await axios.get(coinGeckoUrl)
 			return res.send({ data: { fialValue: response.data, token: tokenItems } })
 		}
