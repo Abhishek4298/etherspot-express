3cbf0819de0281d74c80b4917fbd28a0835299f8
Abhishek4298 <abhishek.bhavsar@bacancy.com>
2023-01-09 23:52:20 +0530

    Tokens and Currency related information get from etherspot and coinGecko.

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const PORT = process.env.PORT || 3000;
-const axios = require('axios');
+require('dotenv').config()
+const express = require('express')
+const { Sdk } = require('etherspot')
+const app = express()
+const etherspot = new Sdk(process.env.API_KEY)
+const axios = require('axios')
 
-app.use(cors());
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello Etherspot!')
+app.get('/tokens', async (req, res) => {
+	// get dynamic currency
+	const currency = req.query.currency
+	try {
+		// get all tokens from etherspot
+		const tokens = await etherspot.getTokenListTokens()
+		let tokenItems = tokens.map((t) => t.symbol).splice(0,500)
+
+		if (tokenItems.length) {
+			// coinGecko URL to get dynamic currency record
+			const coinGeckoUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${tokenItems.join(',')}&vs_currencies=${currency}`
+			const response = await axios.get(coinGeckoUrl)
+			return res.send({ data: { fialValue: response.data, token: tokenItems } })
+		}
+	} catch (err) {
+		return res.status(500).json({
+			error: err.message,
+		})
+	}
 })
 
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log(`Server is listening at PORT`)
+	console.log(`Server listening on port ${PORT}`)
 })
